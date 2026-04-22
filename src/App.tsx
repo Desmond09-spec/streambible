b6c3f64fa9f71e34876ceff7ac1b33c91c7bb682
Desmond09-spec <danieloluwawemimo@gmail.com>
2026-04-22 08:02:43 +0100

    Fixed build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ControllerPage from './pages/ControllerPage';
 import OverlayPage from './pages/OverlayPage';
